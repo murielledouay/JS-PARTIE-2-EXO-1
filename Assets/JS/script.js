657b41b7fdce8efadd6abaa3a38fc3b7e50a97bb
murielledouay <guyard_murielle@orange.fr>
2020-05-04 14:44:34 +0200

    JS PARTIE 2 EXO 1

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,6 @@
-function changeImage(img){
-  document.getElementById('Assets/img/Voiture 1.png').src =img;
+function changeImage(){
+  document.getElementById('greenCar').src ='Assets/img/Voiture 2.png';
+}
+function originImage(){
+  document.getElementById('greenCar').src ='Assets/img/Voiture 1.png';
 }
